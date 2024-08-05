d32781c52bbfc7ba334cfbe13c8e02790a8c2d85
Andrea Natale <andreanatale85@proton.me>
2024-08-05 15:48:51 +0200

    Updates

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,11 +1,34 @@
 "use client"
 
-import useRSSFeed from "../hooks/useRSSFeed";
+import { useEffect, useState } from "react";
 import Feed from "./components/Feed";
+import { proxy } from "../routes/api/rssProxy";
 
 export default function Blog() {
-    const url = 'https://medium.com/feed/@andreanatale85';
-    const { feed, error, isLoading } = useRSSFeed(url);
+    const url = 'https://medium.com/feed/@andreanatale85'
+    const proxyUrl = `/routes/api/rssProxy?url=${url}`;
+    const [feed, setFeed] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetchToRssProxy()
+    }, [])
+
+    // Funzione che esegue la chiamata al proxy
+    const fetchToRssProxy = async () => {
+        try {
+            const response = await fetch(proxyUrl);
+            if (!response.ok) {
+                setError(`Errore nella rihiesta al proxy: ${response.statusText}`)
+            }
+            const text = await response.text();
+            setFeed(text)
+        } catch (error) {
+            console.error(`Errore durante la richiesta al feed RSS: ${error}`)
+            setError(error)
+        }
+    }
     return (
         <main className="mt-4 flex flex-col gap-4">
             <Feed feed={feed} error={error} isLoading={isLoading} />
