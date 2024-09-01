3117acccff938da65be74e26e07f2812482dd63e
Andrea Natale <andreanatale85@proton.me>
2024-09-01 14:54:42 +0200

    Updates

--- app/page.js
+++ app/page.js
@@ -3,8 +3,6 @@
 import Memory from "./contents/memory_game/Memory";
 import Info from "./contents/info/Info";
 import Potential from "./contents/potential/Potential";
-import Stack from "./contents/stack/Stack";
-import TryVideogame from "./contents/tryVideogames/TryVideogames";
 import Who from "./contents/who/Who";
 import TapGame from "./contents/tap_game/TapGame";
 import LinkedinLink from "./contents/linkedin/LinkedinLink";
@@ -13,7 +11,6 @@ import Instagram from "./contents/instagram/Instagram";
 import Email from "./contents/email/Email";
 import DragGame from "./contents/dragGame/DragGame";
 import Makinario from "./contents/makinario/Makinario";
-import { useState } from "react";
 
 const componentsList = [
     <Who isBlack />,
