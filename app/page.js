b30a942d0055b809a36ed02e4259fa09991e3388
Andrea Natale <andreanatale85@proton.me>
2024-09-02 11:20:16 +0200

    Updates

--- app/page.js
+++ app/page.js
@@ -9,7 +9,6 @@ import Instagram from "./contents/instagram/Instagram";
 import Email from "./contents/email/Email";
 import DragGame from "./contents/dragGame/DragGame";
 import Makinario from "./contents/makinario/Makinario";
-import { useState } from "react";
 
 const componentsList = [
     <Who isBlack />,
@@ -26,11 +25,10 @@ const componentsList = [
 ]
 
 export default function Home() {
-const [components] = useState(componentsList)
     return (
         <main className="mt-4 flex flex-col gap-4 lg:gap-0">
             {
-                components && components.map((element, index) => (
+                componentsList && componentsList.map((element, index) => (
                     <div key={index}>{element}</div>
                 ))
             }
