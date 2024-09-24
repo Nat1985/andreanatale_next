5d01efcab8f3174f18fe4557bcfb16ca3f71b218
Andrea Natale <andreanatale85@proton.me>
2024-09-24 23:58:21 +0200

    Black background in videogames section

--- app/videogames/page.js
+++ app/videogames/page.js
@@ -23,7 +23,7 @@ export default function VideogamePortfolio() {
 
     return (
         <main className="mt-4 flex flex-col gap-4">
-            <Panel>
+            <Panel isBlack>
                 <div className="w-full flex flex-col gap-8">
                     {
                         videogames &&
