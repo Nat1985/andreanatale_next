97d7b24e00540436a2f22e5b043ad97a38d71d3c
Andrea Natale <andreanatale85@proton.me>
2024-10-08 10:03:02 +0200

    Updates

--- app/contact/components/ConditionalContact.jsx
+++ app/contact/components/ConditionalContact.jsx
@@ -18,32 +18,35 @@ const ConditionalContact = () => {
     return (
         <Panel>
             {
-                getContact ? (
-                    <div className="flex flex-col gap-8 items-center text-center pt-16">
-                        <h2 className="text-pink-500">Complimenti!</h2>
-                        <h3>Hai collezionato tutte le medagliette :)</h3>
-                        <div className="flex gap-1 p-2 border-2 border-dashed border-slate-300 rounded-xl">
-                            <Image src="/icons/full_badge.png" width={48} height={48} />
-                            <Image src="/icons/full_badge.png" width={48} height={48} />
-                            <Image src="/icons/full_badge.png" width={48} height={48} />
-                            <Image src="/icons/full_badge.png" width={48} height={48} />
-                            <Image src="/icons/full_badge.png" width={48} height={48} />
-                        </div>
-                        <p>La gamification è un'arma molto potente per catturare l'attenzione degli utenti.</p>
-                        <p>Facendo giocare gli utenti si può raggiungere qualsiasi obiettivo. Se hai qualche idea (o se vuoi essere aiutato a trovare quella giusta per la tua azienda):</p>
-                        <ContactForm />
-                    </div>
-
-                ) : (
-                    <div className="flex flex-col gap-8 items-center text-center pt-16">
-                        <Image src="/icons/stop.png" width={128} height={128} />
-                        <h3 className="text-pink-500">Non puoi ancora contattarmi</h3>
-                        <h5>Non hai collezionato abbastanza medagliette!</h5>
+                getContact &&
+                <div className="flex flex-col gap-8 items-center text-center pt-16">
+                    <h2 className="text-pink-500">Complimenti!</h2>
+                    <h3>Hai collezionato tutte le medagliette :)</h3>
+                    <div className="flex gap-1 p-2 border-2 border-dashed border-slate-300 rounded-xl">
                         <Image src="/icons/full_badge.png" width={48} height={48} />
-                        <p>Torna indietro, risolvi i mini-game e naviga il sito per ottenerle. Poi torna qui :)</p>
+                        <Image src="/icons/full_badge.png" width={48} height={48} />
+                        <Image src="/icons/full_badge.png" width={48} height={48} />
+                        <Image src="/icons/full_badge.png" width={48} height={48} />
+                        <Image src="/icons/full_badge.png" width={48} height={48} />
                     </div>
-                )
+                    <p>La gamification è un'arma molto potente per catturare l'attenzione degli utenti.</p>
+                    <p className="mb-8">Facendo giocare gli utenti si può raggiungere qualsiasi obiettivo. Se hai qualche idea (o se vuoi essere aiutato a trovare quella giusta per la tua azienda):</p>
+
+                </div>
             }
+            {
+                !getContact &&
+                <div className="flex flex-col gap-8 items-center text-center pt-16">
+                    <Image src="/icons/empty_badge.png" width={48} height={48} />
+                    <h3 className="text-pink-500">Ti mancano ancora medagliette da collezionare!</h3>
+                    <h4>Torna a navigare il sito per collezionarne altre</h4>
+                    <p className="mb-8">oppure</p>
+                </div>
+            }
+
+
+            <ContactForm />
+
         </Panel>
     )
 }
