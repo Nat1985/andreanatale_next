5491f2a4385db11bfba112fae14c223db89533da
Andrea Natale <andreanatale85@proton.me>
2024-11-11 18:12:58 +0100

    Updates

--- app/contents/who/Who.jsx
+++ app/contents/who/Who.jsx
@@ -21,14 +21,6 @@ const Who = ({ isBlack }) => {
                 Sviluppo <b>applicativi web</b> moderni e sartoriali per <b>liberi professionisti</b> e <b>piccole aziende</b>.
             </p>
 
-            <div className='flex justify-center mt-16'>
-                <Image src={scissor} alt="creative developer" width={96} height={96} className='rotate-[40deg]' />
-                <div className='w-[300px] flex flex-col justify-center'>
-                    <div className='w-full border-b-4 border-dashed border-gray-400'></div>
-                </div>
-            </div>
-
-
 
         </Panel>
     )
