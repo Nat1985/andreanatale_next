6c0b57102c3bea4c23b26cf1cb5635c9ec222410
Andrea Natale <andreanatale85@proton.me>
2024-11-13 10:21:02 +0100

    WorkCard layout updated

--- app/components/sections/works/WorkCard.jsx
+++ app/components/sections/works/WorkCard.jsx
@@ -5,7 +5,7 @@ function WorkCard({data}) {
     return (
         <div className='flex flex-col lg:flex-row sm:items-center justify-center'>
             {/* IMAGE */}
-            <div><Image src={data.image} width={512} height={512} layout='responsive' className='rounded-3xl sm:max-w-[500px] sm:min-w-[340px]' /></div>
+            <div className='rounded-3xl overflow-hidden'><Image src={data.image} width={512} height={512} layout='responsive' className='sm:max-w-[500px] sm:min-w-[340px] transform transition duration-300 hover:scale-110 ease-in-out' /></div>
             {/* INFO */}
             <div className='bg-white backdrop-blur-sm bg-opacity-40 rounded-3xl p-8 sm:max-w-[400px] mt-[-40px] lg:ml-[-40px] flex flex-col gap-2 md:shadow-lg'>
                 <h3 className='hidden sm:block'>{data.title}</h3>
